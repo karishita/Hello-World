cd4ffcd4f1a5b7961b9d9ee123dd730971b261e3
karishita <karishita025@gmail.com>
2024-04-27 16:55:11 +0530

    first commit

---
+++ test1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class test1 {
+    public static void main(String args[])
+    {
+           Scanner sc=new Scanner(System.in);
+           int a;
+           System.out.println("Enter a number");
+           a=sc.nextInt();
+           System.out.println("The number is"+a);
+
+    }
+    
+}
---
+++ test2.java
@@ -0,0 +1,15 @@
+public class test2 {
+    public static void main(String args[])
+    {
+        switch(4+5)
+        {
+            case 4+5:
+            System.out.println("this is case 0");
+            break;
+
+            default:
+            System.out.println("This is default case");
+            break;
+        }
+    }
+}
